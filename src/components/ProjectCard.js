f692c78eec3df1c38db9ccc6c554527547da35b7
Julianb18 <julian141718@gmail.com>
2021-07-04 15:31:08 +0200

    projects html structure

---
+++ src/components/ProjectCard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ProjectCard() {
+  return (
+    <div>
+      <h1>hi</h1>
+    </div>
+  )
+}
+
+export default ProjectCard
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,11 +1,45 @@
-import React from 'react';
+import React from 'react'
+
+import nature from '../utils/images/nature.jpg'
+import style from '../styles/Projects.module.css'
 
 function Projects() {
   return (
-    <div>
-      <h2>Projects</h2>
+    <div className={style.projects}>
+      <div className={style.projectsWrapper}>
+        <div className={style.title}>
+          <span>Take a look at my</span>
+          <h2>Projects</h2>
+        </div>
+        <div className={style.cardContainer}>
+          <div className={style.projectCard}>
+            <div className={style.projectImage}>
+              <img src={nature} alt='' />
+            </div>
+            <div className={style.projectDetails}>
+              <div className={style.projectTitle}>
+                <h3>title</h3>
+                <p>
+                  <span>Tech used:</span> React, something ...
+                </p>
+              </div>
+              <div className={style.projectText}>
+                <ul>
+                  <li>some info</li>
+                  <li>more info</li>
+                  <li>and a little more info</li>
+                </ul>
+              </div>
+              <div className={style.btnsContainer}>
+                <button>Live</button>
+                <button>Repo</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default Projects;
+export default Projects
